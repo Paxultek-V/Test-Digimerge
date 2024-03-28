a21483d2b889cb877164eefefc549fad60bc073e
Valentin203 <valentin@smallbeautiful.studio>
2024-03-28 14:30:12 +0100

    Update Spawner_Stats.cs

--- Assets/_Game/Scripts/Actors/Spawner_Values/Spawner_Stats.cs
+++ Assets/_Game/Scripts/Actors/Spawner_Values/Spawner_Stats.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -18,5 +19,37 @@ public class Spawner_Stats : MonoBehaviour
         return 0f;
     }
     
+    
+    
+    
+    /*private IEnumerator BonusCoroutine(BonusType type, float value, float duration)
+    {
+        switch (type)
+        {
+            case BonusType.SpawnFrequency:
+                m_currentSpawnSpeed += value;
+                break;
+            case BonusType.BoostInitialValue:
+                UpdateValueToSpawn(m_currentValueToSpawn + value);
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(type), type, null);
+        }
+
+        yield return new WaitForSecondsRealtime(duration);
+
+        switch (type)
+        {
+            case BonusType.SpawnFrequency:
+                m_currentSpawnSpeed -= value;
+                break;
+            case BonusType.BoostInitialValue:
+                UpdateValueToSpawn(m_currentValueToSpawn - value);
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(type), type, null);
+        }
+    }*/
+
     
 }
